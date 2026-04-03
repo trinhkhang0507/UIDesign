dbb77ad21f459ce4d7b9ce76f9cb6c3af8c1ac09
Trịnh Đăng Khang <trinhkhang0507@gmail.com>
2026-04-03 13:32:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("components/header.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+
+    fetch("components/footer.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
+
+const slider = document.querySelector(".slider");
+
+if (slider) {
+  const slides = document.querySelectorAll(".slider img");
+
+  const firstClone = slides[0].cloneNode(true);
+  slider.appendChild(firstClone);
+
+  let index = 0;
+  const total = slider.children.length;
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  setInterval(() => {
+    index++;
+    updateSlider();
+
+    if (index === total - 1) {
+      setTimeout(() => {
+        slider.style.transition = "none";
+        index = 0;
+        updateSlider();
+
+        setTimeout(() => {
+          slider.style.transition = "transform 0.5s ease";
+        }, 50);
+      }, 500);
+    }
+  }, 3000);
+
+  const next = document.querySelector(".next");
+  const prev = document.querySelector(".prev");
+
+  if (next) next.onclick = () => {
+    index = (index + 1) % total;
+    updateSlider();
+  };
+
+  if (prev) prev.onclick = () => {
+    index = (index - 1 + total) % total;
+    updateSlider();
+  };
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const minus = document.querySelector(".minus");
+  const plus = document.querySelector(".plus");
+  const input = document.querySelector(".qty-input");
+
+  if (minus && plus && input) {
+
+    minus.addEventListener("click", () => {
+      let value = parseInt(input.value) || 1;
+      if (value > 1) input.value = value - 1;
+    });
+
+    plus.addEventListener("click", () => {
+      let value = parseInt(input.value) || 1;
+      input.value = value + 1;
+    });
+
+  }
+
+});
